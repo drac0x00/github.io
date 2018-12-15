8ba95978e89b59f7ba8aea69b9c3762e5350d890
drac0x00 <kurtbugbee@gmail.com>
2018-12-14 21:23:33 -0500

    Added icons

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,26 @@
 <template>
   <div id="app">
     <p>{{ msg }}</p>
+    <p>I'm a Software Engineer in Boston, MA.</p>
     <ul>
       <li>
-        <a href="https://github.com/drac0x00" target="_blank">GitHub</a>
+        <a href="https://github.com/drac0x00" target="_blank">
+          <img src="src/assets/icons/github-logo.svg">
+          drac0x00
+        </a>
       </li>
       <li>
-        <a href="https://kurt.app">Lists</a>
+        <a href="https://kurt.app">
+          <img src="src/assets/icons/checked.svg">
+          Lists
+        </a>
         <i>Coming soon!</i>
       </li>
       <li>
-        <a href="mailto:kurt@kurt.app" target="_blank">Contact</a>
+        <a href="mailto:kurt@kurt.app" target="_blank">
+          <img src="src/assets/icons/sent-mail.svg">
+          Contact
+        </a>
       </li>
     </ul>
     <p>Let's build something together.</p>
@@ -29,7 +39,7 @@ export default {
 </script>
 
 <style lang="scss">
-$font-main: "Helvetica";
+$font-main: "Helvetica Neue";
 $root-container-width: 18rem;
 $root-font-size: 14px;
 
@@ -73,6 +83,12 @@ body {
   max-width: $root-container-width;
 }
 
+img {
+  height: 1.25rem;
+  margin-right: 0.25rem;
+  vertical-align: middle;
+}
+
 p,
 ul {
   margin: {
@@ -81,7 +97,7 @@ ul {
   padding: 0;
 }
 li {
-  font-size: 0.95rem;
+  font-size: 1rem;
   line-height: 1.3rem;
   margin: 0;
   list-style-type: none;
@@ -90,5 +106,6 @@ li {
 
 a {
   color: #0864c7;
+  text-decoration: none;
 }
 </style>
